b752b594c73692c7ec37bf2b3898835dd822efa0
marcela27 <marcela.almeida272006@gmail.com>
2024-02-23 21:40:02 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$nome = "marcinha";
+$idade = 17;
+$lista = ['banana','uva','limão'];
+
+echo $nome .'\n';
+echo $idade .'\n';
+echo $lista;
+echo .'<br>';
+var_dump($nome);
+echo .'\n';
+var_dump($nome);
+echo .'\n';
+var_dump($nome);
+
+
